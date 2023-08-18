7876e4f7204c0a4dc8b49932a5c04ad0c60bc0c1
kimgyutae1313 <134578227+kimgyutae1313@users.noreply.github.com>
2023-08-18 11:42:29 +0900

    Delete sekki directory

--- sekki/src/main/java/com/care/sekki/member/ReviewDAO.java
+++
@@ -1,9 +0,0 @@
-package com.care.sekki.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewDAO extends JpaRepository<ReviewDTO, Long> {
-	
-}
