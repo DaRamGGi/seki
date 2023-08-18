a82ee64bd58c629443d1532e72b963ae6f5a9f40
k-gardn <rladnjs15@naver.com>
2023-08-18 14:17:41 +0900

    임시비밀번호 발급 완료

--- sekki/src/main/java/com/care/sekki/member/SmsService.java
+++ sekki/src/main/java/com/care/sekki/member/SmsService.java
@@ -1,6 +1,5 @@
 package com.care.sekki.member;
 
-
 import org.springframework.stereotype.Service;
 
 import net.nurigo.sdk.NurigoApp;
@@ -33,6 +32,28 @@ public class SmsService {
 
 	}
 
+	public String sendTempPwSms(String phoneNumber, String tempPw) {
+		DefaultMessageService messageService = NurigoApp.INSTANCE.initialize("api키", "api-secret키",
+				"https://api.coolsms.co.kr");
+		Message message = new Message();
+		message.setFrom("01035056792");
+		message.setTo(phoneNumber);
+		message.setText("(자취세끼) 임시비밀번호 입니다. 로그인 후 반드시 비밀번호를 변경하세요."
+				+ "임시비밀번호 [" + tempPw + "]");
+
+		try {
+			messageService.send(message);
+			return "* 입력하신 전화번호로 임시비밀번호를 전송했습니다.";
+		} catch (NurigoMessageNotReceivedException exception) {
+			System.out.println(exception.getFailedMessageList());
+			System.out.println(exception.getMessage());
+			return "* 인증번호 전송을 실패했습니다.";
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+			return "* 인증번호 전송을 실패했습니다.";
+		}
+	}
+
 	public void sendAdminReply(String phoneNumber) {
 		DefaultMessageService messageService = NurigoApp.INSTANCE.initialize("api키", "secret-api키",
 				"https://api.coolsms.co.kr");
@@ -50,4 +71,5 @@ public class SmsService {
 			System.out.println(exception.getMessage());
 		}
 	}
+
 }
